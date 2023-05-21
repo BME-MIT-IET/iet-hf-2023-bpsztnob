db259b6301350e016876a03781366a5b0dafe2bf
adaz008 <adi.szigeti@gmail.com>
2023-05-21 13:33:03 +0200

    Fixed bug, made Steppable Serializable

--- src/main/java/projlab/Steppable.java
+++ src/main/java/projlab/Steppable.java
@@ -1,9 +1,11 @@
 package projlab;
 
+import java.io.Serializable;
+
 /**
  * Egy interfész, ami minden olyan dolgot reprezentál, amely időben lépni tud.
  */
-public interface Steppable {
+public interface Steppable extends Serializable{
     /**
      * Az adott lépésben végrehajtandó művelet.
      */
